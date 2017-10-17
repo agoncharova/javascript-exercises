5be557f93eb387d87a7bed081f0da5ff6dece9ef
agoncharova <agonchar@bu.edu>
2017-10-17 17:05:27 -0400

    Add some fun code

--- debug.js
+++ debug.js
@@ -1,11 +1,25 @@
-
-
-var loopToFive = function() {
+/*
+var caseOne = function() {
   for (var i = 0, i < 5, i++) {
     console.log(i);
   }
 }
+*/
 
+/*
+var caseTwoHelper = function(){
+   var thing = "foo";
+}
+
+var caseTwo = function(){
+  caseTwoHelper();
+	thing;
+}
+*/
+
+/*
 $("#debug-button").click(function(event) {
-	loopToFive();
+	// caseOne();
+	// caseTwo();
 });
+*/
--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 $('.kale-radio').click(function(event) {
-	var radioId = event.target.id;
 	var response = event.target.value;
 	if(response){ 
 		alert("You like kale.");
 	} else {
 		alert("You are not a huge fan of kale.");
 	}
-})
+});
+
+/*
+var testFuncTwo = function(bool) {	
+	console.log("Awesome job!");
+}
+
+var testFuncThree = function(bool) {
+	console.log("Yey, test function three");
+}
+
+var testFuncOne = function(bool) {
+	if(bool) {
+		testFuncTwo();
+	} else {
+		testFuncThree();
+	}
+}
+
+var pickRandomBoolean = function() {
+	var randomNumber = Math.random();
+	if(randomNumber < 0.50){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+$('#debug-button').click(function(event) { 
+	 var randomBoolean = pickRandomBoolean();
+	 debugger;
+	 testFuncOne(randomBoolean);
+});
+*/
